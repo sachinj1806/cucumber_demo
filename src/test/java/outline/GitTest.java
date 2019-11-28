1ddf32f9219b72ca97e98002014a332fa501d23a
unknown <rj89862@gmail.com>
2019-11-28 11:34:21 +0530

    Testing git commands

---
+++ src/test/java/outline/GitTest.java
@@ -0,0 +1,11 @@
+package outline;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added this class file for git commit demo");
+
+	}
+
+}
